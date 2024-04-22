f5a78b91268b91b8017a758d74562c92b259b1f1
Nathan Sinaga <if21043@students.ithb.ac.id>
2024-04-22 14:13:50 +0700

    Added Menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.11.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.drawerlayout:drawerlayout:1.1.1")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
